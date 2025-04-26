e638fc36cedebfaedce45297b93b53a72deb351c
speed382286 <speed382286@gmail.com>
2025-04-27 02:57:59 +0900

    snap(auto): 04-27_02:57

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 }
 
 export default nextConfig; 
