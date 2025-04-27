dd834dec5e95003ef30d8b182b080f7e4dbab80a
speed382286 <speed382286@gmail.com>
2025-04-27 19:42:30 +0900

    snap(auto): 04-27_19:42

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
+  // output: 'export', // Commented out for Vercel deployment
   images: {
     unoptimized: true
   },
